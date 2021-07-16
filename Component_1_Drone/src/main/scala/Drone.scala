580db40b7d039e8052d9ce7acc90d8eeee090359
RayanFRAJ <r.ferouj@gmail.com>
2021-07-16 14:32:31 +0200

    First try producer (not working) 	modified:   ../Component_1_Drone/src/main/scala/Drone.scala 	deleted:    ../Component_1_Drone/src/main/scala/Producer.scala 	deleted:    ../Component_1_Drone/src/main/scala/kafka.scala

--- Component_1_Drone/src/main/scala/Drone.scala
+++ Component_1_Drone/src/main/scala/Drone.scala
@@ -1,29 +1,75 @@
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
+import org.apache.kafka.common.serialization.StringSerializer
 import org.joda.time.DateTime
 
+import java.util.Properties
 import scala.annotation.tailrec
 import scala.collection.immutable.LazyList
 import scala.language.postfixOps
 import scala.util.Random
-import scala.util.Random.{between, nextDouble}
+import scala.util.Random.between
+
 
 object Drone {
-  def main(args: Array[String]): Unit = {
-    print(generateReport(5).toString)
-    // (generateSentence take between(2, 100)).foreach(x => println(x.mkString trim))
+  def main(args: Array[String]) : Unit = {
 
+
+    val alertProperty: Properties = new Properties()
+
+    alertProperty.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    /* Name of the citizen  */
+    alertProperty.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+    /* Geographic Localization */
+    alertProperty.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+
+    val alertProducer: KafkaProducer[String, String] = new KafkaProducer[String, String](alertProperty)
+
+    val report: Report = generateReport(1)
+    println(report.toString)
+    processReport(alertProducer, report)
+
+    alertProducer.close()
    // generateWord take between(2, 25)
   }
 
   /**
+   * Hidden loop in order to launch a simulated drone
+   *
+   * @param i Integer
+   */
+ /* @tailrec
+  def run(i: Int): Unit = {
+    val id: Int = 1
+    i match {
+      case 1 =>
+        println("Starting up the Drone POC simulation")
+        generateReport(id)
+        run(i + 1)
+      case i if i % 50 == 0 =>
+        println("The drone is generating a report, sending is imminent")
+        processReport(generateReport(id))
+        run(i + 1)
+    }
+  }*/
+
+  def processReport(producer: KafkaProducer[String, String], r: Report) : Unit = {
+    // Checks for negatives scores and prepare the Alert
+    val processedAlert : List[(String, Int)] = r.citizenInVicinity.collect{
+      case x if x._2 < 0 => x
+    }
+    processedAlert.foreach(x => sendAlert(producer, "Alert", x._1, x._2, r.position.toString()))
+  }
+
+  /**
    * Function that generate a report
-   * @param id
+   * @param id default parameter : 1
    */
   def generateReport(id : Int): Report = {
-    val date = generateTimestamp()
-    val position = generateCurrentLocation()
-    val citizenInVicinity = ((generateNameCitizen take between(2, 25)) toList)
-    val citizenWithScore = assignPeaceScore(citizenInVicinity)
-    val words = (generateWord take between(2, 25) toList)
+    val date: DateTime = generateTimestamp()
+    val position: (Double, Double) = generateCurrentLocation()
+    val citizenInVicinity: List[String] = (generateNameCitizen take between(2, 25)) toList
+    val citizenWithScore: List[(String, Int)] = assignPeaceScore(citizenInVicinity)
+    val words: List[String] = (generateWord take between(2, 25) toList)
     new Report(date, 1, position, citizenWithScore, words)
   }
 
@@ -32,7 +78,7 @@ object Drone {
    * @return
    */
   def generateWord: LazyList[String] = {
-    def word : String = {
+    def word: String = {
       (Random.alphanumeric take between(2, 10)).mkString
     }
     LazyList continually word
@@ -53,7 +99,7 @@ object Drone {
    * Generate a GPS coordinate tuple (Latitude, Longitude)
    * @return
    */
-  def generateCurrentLocation() : (Double, Double) = {
+  def generateCurrentLocation(): (Double, Double) = {
     (between(-90.0, 90.0), between(-180.0, 180.0))
   }
 
@@ -61,8 +107,8 @@ object Drone {
    * Generate a single word defining name from a LazyList of alphanumeric
    * @return
    */
-  def generateNameCitizen() : LazyList[String] = {
-    def word : String = {
+  def generateNameCitizen(): LazyList[String] = {
+    def word: String = {
       (Random.alphanumeric take between(2, 10)).mkString
     }
     LazyList continually word
@@ -75,11 +121,11 @@ object Drone {
    * Neutral 0 integer is for neutral behaviour
    * @return Int
    */
-  def generatePeaceScore() : Int = {
+  def generatePeaceScore(): Int = {
     between(-42, 42)
   }
 
-  def assignPeaceScore(l : List[String]) : List[(String, Int)] = {
+  def assignPeaceScore(l : List[String]): List[(String, Int)] = {
     l.map(name => (name, generatePeaceScore()))
   }
 
@@ -87,7 +133,17 @@ object Drone {
    * Generate the current local TimeStamp using DateTime
    * @return
    */
-  def generateTimestamp() : DateTime = {
+  def generateTimestamp(): DateTime = {
     DateTime.now()
   }
+
+  /* Alert Support Producer */
+
+  def sendAlert(alertProducer: KafkaProducer[String, String], topic: String, name: String, score: Int, location: String): Unit = {
+    val stringConcat = location.concat(",").concat(score.toString)
+    val recordAlert = new ProducerRecord[String, String](topic, name, stringConcat)
+    alertProducer.send(recordAlert)
+    println(s"[$topic] The drone has sent alert for $name located at $location with a score of $score")
+  }
+
 }
