356689052cf12aac918dc0b049cb7d3130cbfabb
NoobZik <raki_b@live.fr>
2021-07-25 23:49:34 +0200

    Spark / Kafka Integration consumer for Data Storage (not working)

--- Component_3_Distributed_Storage/src/main/scala/Storage.scala
+++ Component_3_Distributed_Storage/src/main/scala/Storage.scala
@@ -1,54 +1,62 @@
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010._
+//import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming._
+
 import java.time.Duration
-import java.util.{Collections, Properties}
+import java.util.Properties
 import scala.annotation.tailrec
 import scala.collection.convert.ImplicitConversions.`iterable AsScalaIterable`
 
 
-
 object Storage {
     def main(args: Array[String]): Unit = {
-        val topic = "Reports"
+        val topic = Array("Reports")
+
+        val kafkaProb = Map[String, Object](
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+            ConsumerConfig.GROUP_ID_CONFIG -> "consumer-group-data-storage",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest",
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (false: java.lang.Boolean)
+        )
 
 
-        val props = new Properties()
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-data-storage")
 
-        val consumer = new KafkaConsumer[String, String](props)
-        consumer.subscribe(Collections.singletonList(topic))
+        val spark = SparkSession.builder
+          .master("local[2]")
+          .appName("Peacekeeper_Storage")
+          .getOrCreate();
 
-        manageConsumer(consumer, 1000,1)
-        consumer.close()
+        val sc = spark.sparkContext
+
+        val streamingContext = new StreamingContext(sc, Seconds(5))
+
+        val stream = KafkaUtils.createDirectStream[String, String](
+            streamingContext,
+            PreferConsistent,
+            Subscribe[String, String](topic, kafkaProb)
+        )
+
+        stream.foreachRDD(line => {
+            if(!line.isEmpty()) {
+                val rdd : RDD[String] = line.map(_.value.toString)
+                import spark.implicits._
+                val df = spark.read.json(spark.createDataset(rdd))
+                df.show()
+                df.printSchema()
+            }
+        })
+
+        streamingContext.start()
+        streamingContext.awaitTermination()
+
     }
 
-
-    /**
-     * Retrieves and print any received poll from the subscribed consumer (topic alert)
-     * @param consumer A KafkaConsumer[String, String]
-     * @param consumerLifetime An Integer
-     * @param pollDuration An Integer
-     */
-    @tailrec
-    def manageConsumer(consumer: KafkaConsumer[String, String], consumerLifetime: Int, pollDuration: Int): Unit = {
-        if (consumerLifetime != 0) {
-            val polledRecords = consumer.poll(Duration.ofSeconds(pollDuration))
-            if (!polledRecords.isEmpty) {
-                println(s"Polled ${polledRecords.count()} records")
-                polledRecords.foreach(record => {
-                    println(s"ALERT : A Peacemaker is required at geographical location ${record.value()} for the citizen ${record.key()} !")
-                })}
-            manageConsumer(consumer, consumerLifetime - pollDuration, pollDuration)
-        }
-    }
-
-    // Todo Integrate Kafka and Spark
-    // Retrieve Data Report
-    // Convert it to Spark
-    // Store it
-    def dataStorage(test: Int):  Unit = {
-    }
 }
