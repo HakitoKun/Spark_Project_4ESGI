a44d5a62efde8bea08821295759052c0e6b7dfd8
NoobZik <raki_b@live.fr>
2021-07-27 20:22:33 +0200

    Analysis DF cleaning functional

--- Component_4_Analysis/src/main/scala/Analysis.scala
+++ Component_4_Analysis/src/main/scala/Analysis.scala
@@ -1,13 +1,5 @@
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.functions.{concat_ws, lit}
-import org.apache.spark.sql.{Column, SparkSession, functions}
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010._
-import java.util.logging.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 
 
@@ -15,7 +7,28 @@ object Analysis {
 
 
     def main(args: Array[String]): Unit = {
-   
+        val spark = SparkSession.builder
+          .master("local[2]")
+          .appName("Analysis")
+          .getOrCreate();
+        import spark.implicits._
+        val df_json = spark.read.option("header", "true").json("../Saved_Data/Data_Reports.json/*.json")
+
+
+        val df1 = df_json.withColumn("elem", explode(col("citizenInVicinity")))
+
+        val df2 = df1.withColumn("name", $"elem._1")
+          .withColumn("score", $"elem._2")
+          .withColumn("longitude", col("position._1$mcD$sp"))
+          .withColumn("latitude", col("position._2$mcD$sp"))
+          .withColumn("test", from_unixtime(unix_timestamp($"date")))
+          .withColumn("Date", date_format(col("test"), "yyyy-MM-dd"))
+          .withColumn("Time", date_format(col("test"), "HH:mm:ss"))
+          .select($"drone_id", $"Date", $"Time", $"longitude", $"latitude", $"name", $"score", $"words")
+        df2.show()
+        df2.printSchema()
+
+        println("\n\n\n\n")
 
     }
 
